03c59291785dd4ab55539b3751258b062cf1f939
aytarozgur <zrhaytar@gmail.com>
2021-12-05 02:12:14 +0300

    controller update added

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -51,4 +51,19 @@ class TaskController extends Controller
 
     return redirect('/');
   }
+
+  /**
+   * Update the specified task in storage. For this case, just toggle task's done status.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Task  $task
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, Task $task)
+  {
+      $task->done = !$task->done;
+      $task->save();
+
+      return redirect('/');
+  }
 }
